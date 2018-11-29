f6f7e209e680ae068181b7e36a11a0c1638bf85a
simondlevy <simon.d.levy@gmail.com>
2018-11-29 13:53:31 -0500

    Working with PS4 controller on Ubuntu 18.04

--- pysticks/__init__.py
+++ pysticks/__init__.py
@@ -137,7 +137,7 @@ controllers = {
     '2In1 USB Joystick'                                  : _Playstation((-1,2,-3,0)),
     'Wireless Controller'                                : _Playstation((-1,2,-3,0)),
     'MY-POWER CO.,LTD. 2In1 USB Joystick'                : _Playstation((-1,2,-3,0)),
-    'Sony Interactive Entertainment Wireless Controller' : _Playstation((-1,2,-5,0)),
+    'Sony Interactive Entertainment Wireless Controller' : _Playstation((-1,3,-4,0)),
     'Logitech Extreme 3D'                                : _GameController((-2,0,-1,3), 0),
     'Logitech Logitech Extreme 3D'                       : _GameController((-3,0,-1,2), 0),
     'FrSky Taranis Joystick'                             : _RcTransmitter((0,1,2,5), 3),
