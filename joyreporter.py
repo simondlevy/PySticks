5febf7649686f88577b9070b77410396674ffc2e
simondlevy <simon.d.levy@gmail.com>
2024-01-14 16:29:28 -0500

    joyreporter uses \r to avoid linefeed

--- joyreporter.py
+++ joyreporter.py
@@ -9,16 +9,15 @@ Copyright (C) Simon D. Levy 2018
 This file is part of PySticks.
 
 PySticks is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as 
-published by the Free Software Foundation, either version 3 of the 
-License, or (at your option) any later version.
-This code is distributed in the hope that it will be useful,     
-but WITHOUT ANY WARRANTY without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+it under the terms of the GNU Lesser General Public License as published by the
+Free Software Foundation, either version 3 of the License, or (at your option)
+any later version.  This code is distributed in the hope that it will be
+useful,     but WITHOUT ANY WARRANTY without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+Public License for more details.
 
-You should have received a copy of the GNU Lesser General Public License 
-along with this PySticks.  If not, see <http:#www.gnu.org/licenses/>.
+You should have received a copy of the GNU Lesser General Public License along
+with this PySticks.  If not, see <http:#www.gnu.org/licenses/>.
 '''
 
 from sys import stdout
@@ -43,4 +42,4 @@ while True:
     stdout.write(' | Buttons: ')
     for k in range(controller.get_numbuttons()):
         stdout.write('%d:%d ' % (k, controller.get_button(k)))
-    stdout.write('\n')
+    stdout.write('\r')
